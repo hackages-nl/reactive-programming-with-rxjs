940d4d376b7a8b4354f7e7c9615fdda43bb2638b
Davy Engone <davy@hackages.io>
2024-04-30 10:07:14 +0200

    part 1-fromEvent

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
 
+import { fromEvent } from "./lib/fromEvent";
+import { btnCancel, btnStart, millisecondsToStr, timerDisplay } from "./utils";
+
+/// create an observable
+const timer$ = fromEvent(btnStart, "click");
+
+const subscription = timer$.subscribe({
+    next(value: number){
+        // timerDisplay.innerHTML = millisecondsToStr(value);
+        console.log(value)
+    },
+    error(){},
+    complete(){},
+})
+
+// fromEvent(btnCancel, "click").subscribe({
+//     next(){
+//         subscription.unsubscribe()
+//     }
+// })
+
+
+
--- src/lib/fromEvent.ts
+++ src/lib/fromEvent.ts
@@ -2,10 +2,34 @@
  * @jest-environment jsdom
  */
 
+/**
+ * @jest-environment jsdom
+ */
+
+import { Observer } from "../types";
+
 export function fromEvent(
   el: HTMLElement,
   eventName: "click" | "change" | "error" | "input"
 ) {
 
-  throw new Error("Not implemented");
+  return {
+    subscribe(observer: Partial<Observer>){
+
+     try {
+      el.addEventListener(eventName, observer.next);
+
+      return {
+        unsubscribe(){
+          el.removeEventListener(eventName, observer.next);
+          observer.complete();
+        }
+      }
+     } catch (error) {
+      observer.error("You should provide a valid html element"); 
+      observer.complete();
+     } 
+    }
+  }
 }
+
