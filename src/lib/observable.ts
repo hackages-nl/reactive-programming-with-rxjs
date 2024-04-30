f1fec7c6dceff4b65cdc42620aba4222de704605
Davy Engone <davy@hackages.io>
2024-04-30 10:40:00 +0200

    implemented observable

--- src/lib/observable.ts
+++ src/lib/observable.ts
@@ -1,3 +1,9 @@
+import { Observer, Producer } from "../types";
 
 export class Observable {
+    constructor(public producer: Producer){}
+    
+    subscribe(observer: Observer){
+        return this.producer(observer);
+    }
 }
